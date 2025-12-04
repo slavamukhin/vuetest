1a00823e1db2899184bd76b8eceab8889254490d
smuhin <via4eslav.mukhin@yandex.ru>
2025-12-04 12:16:19 +0300

    github pages fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LayoutView from '@/views/LayoutView.vue';
 import MainView from '@/views/MainView.vue';
 import ShopView from '@/views/ShopView.vue';
@@ -8,7 +8,7 @@ import UserView from '@/views/UserView.vue';
 import FavoriteView from '@/views/FavoriteView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
